507b2cba782c19325c86f2df168db1d158c9dc2e
AIWa <aiwalabro@gmail.com>
2023-04-25 18:16:33 +0530

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # dill and pickle both are used for dump pickle files
 import dill
-# import pickle
+import pickle
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
@@ -16,9 +16,7 @@ from sklearn.model_selection import GridSearchCV
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
-
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
@@ -52,3 +50,11 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param ):
         
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+    
